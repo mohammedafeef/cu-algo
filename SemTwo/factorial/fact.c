9594371f75d125cf65201dcb9f6efbe2030b3015
fazilkp7 <104255104+fazilkp7@users.noreply.github.com>
2022-10-12 12:27:11 +0530

    factorial program

--- SemTwo/factorial/fact.c
+++ SemTwo/factorial/fact.c
@@ -1,6 +1,7 @@
 Problem: <Write a C program to calculate the Factorial of a given number>
 Details: <if statement and for loop are used in this program to make a better program >
 
+Solution:
 
 #include <stdio.h>
 int main() {
