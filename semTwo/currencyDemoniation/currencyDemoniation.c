0824a0059b29d6fc123aa99b135ff9f34810c72c
Mohammed shifin.k <99720958+Mohdshifin@users.noreply.github.com>
2022-10-12 16:30:10 +0530

    changed code

--- semTwo/currencyDemoniation/currencyDemoniation.c
+++ semTwo/currencyDemoniation/currencyDemoniation.c
@@ -1,56 +1,20 @@
 #include<stdio.h>
 #include<conio.h>
+
 void main()
 {
-    int a;
-    printf("Enter an amount : ");
-    scanf("%d",&a);
-    printf("\nCurrency denominations are :\n");
-    if(a/2000!=0)
-    {
-        printf("\n2000 x %d",a/2000);
-        a%=2000;
-    }
-    if(a/500!=0)
-    {
-        printf("\n500 x %d",a/500);
-        a%=500;
-    }
-    if(a/200!=0)
-    {
-        printf("\n200 x %d",a/200);
-        a%=200;
-    }
-    if(a/100!=0)
-    {
-        printf("\n100 x %d",a/100);
-        a%=100;
-    }
-    if(a/50!=0)
-    {
-        printf("\n50 x %d",a/50);
-        a%=50;
-    }
-    if(a/20!=0)
-    {
-        printf("\n20 x %d",a/20);
-        a%=20;
-    }
-    if(a/10!=0)
-    {
-        printf("\n10 x %d",a/10);
-        a%=10;
-    }
-    if(a/5!=0)
-    {
-        printf("\n5 x %d",a/5);
-        a%=5;
-    }
-    if(a/2!=0)
-    {
-        printf("\n2 x %d",a/2);
-        a%=2;
-    }
-    if(a!=0) printf("\n1 x %d",a);
-    getch();
+	int notes[]={2000,500,200,100,50,20,10,5,2,1},amt,i;
+	clrscr();
+	printf("Enter an amount : ");
+	scanf("%d",&amt);
+	printf("\nCurrency denominations are :\n");
+	for(i=0;amt!=0;i++)
+	{
+		if(amt/notes[i]!=0)
+		{
+			printf("\n%4d x %d",notes[i],amt/notes[i]);
+			amt=amt%notes[i];
+		}
+	}
+	getch();
 }
