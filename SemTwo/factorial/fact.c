5dc2d061a81e4da90f4b52622070d10512629c34
fazilkp7 <104255104+fazilkp7@users.noreply.github.com>
2022-10-12 12:26:05 +0530

    Update and rename SemTwo/factorial.c to SemTwo/factorial/fact.c

--- SemTwo/factorial/fact.c
+++ SemTwo/factorial/fact.c
@@ -1,5 +1,5 @@
 Problem: <Write a C program to calculate the Factorial of a given number>
-Details: <statement - if>
+Details: <if statement and for loop are used in this program to make a better program >
 
 
 #include <stdio.h>
