8f63fcfb15578aa89d27421fcb1cf9920007c0db
Mohammed shifin.k <99720958+Mohdshifin@users.noreply.github.com>
2022-10-12 12:37:37 +0530

    added details
    added details

--- semOne/pyramid/pyramid.c
+++ semOne/pyramid/pyramid.c
@@ -6,15 +6,15 @@
 void main()
 {
     int i,j,n;
-    printf("Enter the number of rows : ");
-    scanf("%d",&n);
-    printf("\nPyramid of * with %d rows:\n",n);
-    for(i=1;i<=n;i++)
+    printf("Enter the number of rows : ");  //to enter the row
+    scanf("%d",&n);                         //to read the number
+    printf("\nPyramid of * with %d rows:\n",n); 
+    for(i=1;i<=n;i++)       // using loop printed lines
     {
-        printf("\n");
-        for(j=0;j<n-i;j++)
-        printf(" ");
-        for(j=0;j<2*i-1;j++)
-        printf("*");
+        printf("\n");       //to print a line
+        for(j=0;j<n-i;j++)  //loop to print space
+        printf(" ");        //printed spaces
+        for(j=0;j<2*i-1;j++)    //loop for creating *
+        printf("*");        // * printed
     }
 }
