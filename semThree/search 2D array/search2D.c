c798b03d0c78323268a71f1f3f7f54f6fae6e77d
MhdSalahu <93049081+MhdSalahu@users.noreply.github.com>
2022-10-12 14:21:35 +0700

    Create search2D.c

---
+++ semThree/search 2D array/search2D.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(){
+  int m, n, item, count=0, array[10][10];
+
+  printf("Enter the number of rows and columns: ");
+  scanf("%d %d", &m, &n);
+
+  printf("Enter %d elements: ", (m*n));
+  for(int i=0; i<m; i++){
+    for(int j=0; j<n; j++){
+      scanf("%d", &array[i][j]);
+    }
+  }
+
+  printf("Enter the item to find: ");
+  scanf("%d", &item);
+
+  for(int i=0; i<m; i++){
+    for(int j=0; j<n; j++){
+      if(array[i][j] == item){
+        printf("Item found at [%d, %d] \n", i, j);
+        count++;
+      }
+    }
+  }
+
+  if(count==0)
+    printf("Item Not found");
+
+  return 0;
+}
