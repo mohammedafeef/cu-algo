715aa549915387a5cbf1d88dedecc070e91d1ead
DILSHAD BASITH <87611463+dilshadbasith@users.noreply.github.com>
2022-10-12 12:24:57 +0530

    Reverse string
    created a program to reverse string

---
+++ semTwo/ReverseString/reverse.c
@@ -0,0 +1,15 @@
+problem: <write a c program to reverse a string>
+Details: <It is a simple program to reverse a string>
+
+#include <stdio.h>  
+#include <string.h>  
+int main()  
+{  
+    char str[40]; // declare the size of character string  
+    printf (" \n Enter a string to be reversed: ");  
+    scanf ("%s", str);  
+      
+    // use strrev() function to reverse a string  
+    printf (" \n After the reverse of a string: %s ", strrev(str));  
+    return 0;  
+}  
